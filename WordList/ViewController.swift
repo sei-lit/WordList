336cf3ded283aa42af7c3756df82f97529b1df3a
SeiOmori <sei.omori.22@gmail.com>
2023-05-11 17:33:28 +0900

    Initial Commit

---
+++ WordList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordList
+//
+//  Created by 大森青 on 2023/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
